4e65c50d98012dcc2a633e4259c8d1cc29470023
parkchanjong <7000cj@gmail.com>
2025-08-26 17:23:17 +0900

    set up

---
+++ src/main/java/com/videoservice/manager/VideoInformationManagerApplication.java
@@ -0,0 +1,13 @@
+package com.videoservice.manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideoInformationManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VideoInformationManagerApplication.class, args);
+	}
+
+}
