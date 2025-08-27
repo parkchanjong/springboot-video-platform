884b67601edf22ab816aac6cea5bb207138e7c0c
parkchanjong <7000cj@gmail.com>
2025-08-27 23:35:03 +0900

    feat: mvc 설정 추가

---
+++ video-apps/app-api/src/main/java/com/videoservice/manager/config/WebMVcConfig.java
@@ -0,0 +1,9 @@
+package com.videoservice.manager.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMVcConfig implements WebMvcConfigurer {
+
+}
