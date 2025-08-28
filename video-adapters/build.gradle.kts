89511e0c0d979553c9ed28fcaf765626f27b462b
parkchanjong <7000cj@gmail.com>
2025-08-28 21:53:41 +0900

    feat: redis 설정 추가

--- video-adapters/build.gradle.kts
+++ video-adapters/build.gradle.kts
@@ -5,6 +5,7 @@ dependencies {
 
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-data-mongodb")
+    implementation("org.springframework.boot:spring-boot-starter-data-redis")
     implementation("org.springframework:spring-tx")
 
     implementation("org.flywaydb:flyway-core")
---
+++ video-adapters/src/main/java/com/videoservice/manager/common/MessageTopics.java
@@ -0,0 +1,5 @@
+package com.videoservice.manager.common;
+
+public class MessageTopics {
+    public static final String NEW_VIDEO = "new-video";
+}
