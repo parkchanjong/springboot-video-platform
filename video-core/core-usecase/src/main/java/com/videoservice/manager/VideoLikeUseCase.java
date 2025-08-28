48cca8533ae749f01307428ecbab63576041c63a
parkchanjong <7000cj@gmail.com>
2025-08-28 16:28:52 +0900

    feat: VideoLikeUseCase 추가
    * likeVideo
    * unlikeVideo
    * isLikedVideo
    * getVideoLikeCount

--- video-core/core-usecase/src/main/java/com/videoservice/manager/VideoLikeUseCase.java
+++ video-core/core-usecase/src/main/java/com/videoservice/manager/VideoLikeUseCase.java
@@ -1,5 +1,8 @@
 package com.videoservice.manager;
 
 public interface VideoLikeUseCase {
-
+    Long likeVideo(String videoId, String userId);
+    Long unlikeVideo(String videoId, String userId);
+    Boolean isLikedVideo(String videoId, String userId);
+    Long getVideoLikeCount(String videoId);
 }
