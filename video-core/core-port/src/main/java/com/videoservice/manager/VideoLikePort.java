2128bdb1a2c6599b71a7f310595abeb3af4a4964
parkchanjong <7000cj@gmail.com>
2025-08-28 19:40:26 +0900

    feat: port 추가

--- video-core/core-port/src/main/java/com/videoservice/manager/VideoLikePort.java
+++ video-core/core-port/src/main/java/com/videoservice/manager/VideoLikePort.java
@@ -1,5 +1,11 @@
 package com.videoservice.manager;
 
 public interface VideoLikePort {
+    Long addVideoLike(String videoId, String userId);
 
+    Long removeVideoLike(String videoId, String userId);
+
+    Boolean isVideoLikeMember(String videoId, String userId);
+
+    Long getVideoLikeCount(String videoId);
 }
