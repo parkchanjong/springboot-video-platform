a51f65cce4949e109109c98849d048137332de57
parkchanjong <7000cj@gmail.com>
2025-08-28 16:33:38 +0900

    feat: VideoUseCase 추가
    * getVideo
    * listVideos
    * createVideo
    * increaseViewCount

--- video-core/core-usecase/src/main/java/com/videoservice/manager/VideoUseCase.java
+++ video-core/core-usecase/src/main/java/com/videoservice/manager/VideoUseCase.java
@@ -1,5 +1,15 @@
 package com.videoservice.manager;
 
-public interface VideoUseCase {
+import com.videoservice.manager.command.VideoCommand;
+import com.videoservice.manager.video.Video;
+import java.util.List;
 
+public interface VideoUseCase {
+    Video getVideo(String videoId);
+
+    List<Video> listVideos(String channelId);
+
+    Video createVideo(VideoCommand videoCommand);
+
+    void increaseViewCount(String videoId);
 }
