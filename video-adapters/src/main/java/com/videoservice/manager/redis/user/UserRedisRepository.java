e1aa0c51fa45e881a61cc2bb56333b6101cc6a6f
parkchanjong <7000cj@gmail.com>
2025-09-09 22:56:58 +0900

    feat: user redisHash 제거

--- video-adapters/src/main/java/com/videoservice/manager/redis/user/UserRedisRepository.java
+++
@@ -1,6 +0,0 @@
-package com.videoservice.manager.redis.user;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface UserRedisRepository extends CrudRepository<UserRedisHash, String> {
-}
