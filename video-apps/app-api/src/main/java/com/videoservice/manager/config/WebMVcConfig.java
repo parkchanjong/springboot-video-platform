e7c3cad07b46a9838a4cc2fc640feef4b6c37698
parkchanjong <7000cj@gmail.com>
2025-09-10 00:19:54 +0900

    feat: WebMvc 설정

--- video-apps/app-api/src/main/java/com/videoservice/manager/config/WebMVcConfig.java
+++
@@ -1,9 +0,0 @@
-package com.videoservice.manager.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-@Configuration
-public class WebMVcConfig implements WebMvcConfigurer {
-
-}
---
+++ video-apps/app-api/src/main/java/com/videoservice/manager/config/WebMvcConfig.java
@@ -0,0 +1,22 @@
+package com.videoservice.manager.config;
+
+import com.videoservice.manager.resolver.UserHandlerMethodArgumentResolver;
+import java.util.List;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+    private final UserHandlerMethodArgumentResolver userHandlerMethodArgumentResolver;
+
+    public WebMvcConfig(UserHandlerMethodArgumentResolver userHandlerMethodArgumentResolver) {
+        this.userHandlerMethodArgumentResolver = userHandlerMethodArgumentResolver;
+    }
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        WebMvcConfigurer.super.addArgumentResolvers(resolvers);
+        resolvers.add(userHandlerMethodArgumentResolver);
+    }
+}
