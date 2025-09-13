f5f169c29e74508bf57557aa6ac206154ddb8961
parkchanjong <7000cj@gmail.com>
2025-09-13 23:23:57 +0900

    rename: 모니터링 설정 추가

--- video-adapters/src/main/java/com/videoservice/manager/MessagePersistenceAdapter.java
+++ video-adapters/src/main/java/com/videoservice/manager/MessagePersistenceAdapter.java
@@ -1,22 +1,12 @@
 package com.videoservice.manager;
 
-import com.videoservice.manager.common.MessageTopics;
-import com.videoservice.manager.message.NewVideoMessage;
-import org.springframework.context.annotation.Lazy;
-import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Component;
 
 @Component
 public class MessagePersistenceAdapter implements MessagePort {
-    private final RedisTemplate<String, Object> redisTemplate;
-
-    public MessagePersistenceAdapter(@Lazy RedisTemplate<String, Object> redisTemplate) {
-        this.redisTemplate = redisTemplate;
-    }
 
     @Override
     public void sendNewVideMessage(String channelId) {
-        redisTemplate.convertAndSend(MessageTopics.NEW_VIDEO, new NewVideoMessage(channelId));
-    }
 
+    }
 }
