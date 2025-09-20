b26d7cd8112380d99369242be0705d4ceeadcd60
parkchanjong <7000cj@gmail.com>
2025-09-21 01:03:29 +0900

    feat: coupon 엔티티 추가

---
+++ video-adapters/src/main/java/com/videoservice/manager/jpa/coupon/CouponJpaEntity.java
@@ -0,0 +1,25 @@
+package com.videoservice.manager.jpa.coupon;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity(name = "coupon_policy")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+public class CouponJpaEntity {
+
+    @Id
+    private String id;
+    private String userId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "coupon_policy_id")
+    private CouponPolicyJpaEntity couponPolicy;
+}
---
+++ video-adapters/src/main/java/com/videoservice/manager/jpa/coupon/CouponPolicyJpaEntity.java
@@ -0,0 +1,21 @@
+package com.videoservice.manager.jpa.coupon;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity(name = "coupon")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+public class CouponPolicyJpaEntity {
+
+    @Id
+    private String id;
+    private Integer totalQuantity;
+    private LocalDateTime startTime;
+    private LocalDateTime endTime;
+}
